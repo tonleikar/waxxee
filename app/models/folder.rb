a30cee2cfb26dfcda11403f2a1ea3c775aab07b3
Charles Warburg <251178885+CharlesWarburg@users.noreply.github.com>
2026-03-10 16:14:17 +0000

    collections fix

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -3,4 +3,6 @@ class Folder < ApplicationRecord
   has_many :folder_vinyls, dependent: :destroy
   has_many :user_vinyls, through: :folder_vinyls
   has_many :vinyls, through: :user_vinyls
+
+  validates :name, presence: true
 end
