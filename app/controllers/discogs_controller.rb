6598f90c5b0014bc839b634db055d8153922c12d
Emre Can Koklu <can.koklu@outlook.com>
2026-03-10 15:59:00 +0000

    discogs integration

--- app/controllers/discogs_controller.rb
+++ app/controllers/discogs_controller.rb
@@ -7,5 +7,18 @@ class DiscogsController < ApplicationController
 
     response = Discogs::Client.new.search_releases(query: @query)
     @results = response["results"] || []
+  rescue Discogs::ConfigurationError, Discogs::ApiError => e
+    flash.now[:alert] = e.message
+  end
+
+  def create
+    vinyl = Discogs::ReleaseImporter.new.import!(
+      release_id: params[:release_id],
+      user: current_user
+    )
+
+    redirect_to vinyl_path(vinyl), notice: "Record imported successfully."
+  rescue Discogs::ConfigurationError, Discogs::ApiError, ActiveRecord::RecordInvalid => e
+    redirect_to discogs_path(query: params[:query]), alert: e.message
   end
 end
