78bee730c0b27359a4186ca7c07cc13db2faf598
Charles Warburg <251178885+CharlesWarburg@users.noreply.github.com>
2026-03-11 13:01:20 +0000

    hp major update for randomizer and categories

--- app/controllers/swiper_controller.rb
+++ app/controllers/swiper_controller.rb
@@ -1,6 +1,26 @@
 class SwiperController < ApplicationController
   def index
-    @vinyls = Vinyl.all.sample(20)
+    @persona_key = selected_persona_key || "randomizer"
+    @persona = Persona::RULES.fetch(@persona_key)
+    @vinyls = filtered_vinyl_scope(@persona).to_a.sample(20)
     @user_vinyl = UserVinyl.new
+  end
+
+  private
+
+  def filtered_vinyl_scope(persona)
+    scope = Vinyl.where(year: persona[:min_year]..persona[:max_year])
+
+    return scope if persona[:genres] == [""]
+
+    scope.where(
+      persona[:genres].map { "genre ILIKE ?" }.join(" OR "),
+      *persona[:genres].map { |genre| "%#{genre}%" }
+    )
+  end
+
+  def selected_persona_key
+    key = params[:persona]&.downcase
+    key if key.present? && Persona::RULES.key?(key)
   end
 end
